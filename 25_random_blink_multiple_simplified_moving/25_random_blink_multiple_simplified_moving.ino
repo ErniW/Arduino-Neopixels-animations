96be5dd285f44ff51442557cb993f612e77a0c60
Ernest Warzocha <ernestwarzocha@gmail.com>
2023-06-12 01:49:04 +0200

    multiple moving

--- 25_random_blink_multiple_simplified_moving/25_random_blink_multiple_simplified_moving.ino
+++ 25_random_blink_multiple_simplified_moving/25_random_blink_multiple_simplified_moving.ino
@@ -4,18 +4,18 @@
 #define DATA_PIN 6
 #define GLOBAL_BRIGHTNESS 255
 
-#define BLINK_AMOUNT 5
+#define BLINK_AMOUNT 4
 
 CRGB leds[LED_AMOUNT];
 
 struct Blink{
-  Blink(int size, int pos,  float value){
+  Blink(float size, float pos,  float value){
     this->size = size;
     this->position = pos;
     this->value = value;
   }
-  int position;
-  int size;
+  float position;
+  float size;
   long count = 0;
   float value;
 };
@@ -28,7 +28,7 @@ void setup() {
 
   for(int i=0; i<BLINK_AMOUNT; i++){
 
-    int size = 5;
+    int size = 3;
     int position = random(0, LED_AMOUNT - size);
     int value = random(0, 628) / 100; //To get random between 0 and TWO_PI. Arduino random function returns integers.
 
@@ -51,23 +51,25 @@ void loop() {
       continue;
     }
 
-    blinks[i]->value += 0.05;
+    blinks[i]->value += 0.1;
+    blinks[i]->position += 0.15;
+    blinks[i]->size += 0.2;
 
     if(blinks[i]->value >= TWO_PI){
+      blinks[i]->size = 5;
       blinks[i]->position = random(0, LED_AMOUNT - blinks[i]->size);
       blinks[i]->value = 0;
-      blinks[i]->count = random(500);
+      blinks[i]->count = random(300);
     }
     
   }
-
   FastLED.show();
 }
 
 
 void blinkAnimation(Blink *b){
 
-  float sinStep = TWO_PI / (b->size - 1);
+  float sinStep = TWO_PI / (b->size -1);
 
   float fade = cos(PI + b->value);
   fade = (fade + 1) / 2;
@@ -77,11 +79,13 @@ void blinkAnimation(Blink *b){
     brightness = (brightness + 1) /2;
     brightness *= 255;
     
-    brightness = max(30, brightness);
+    //brightness = max(30, brightness);
     brightness *= fade;
+
+    if((int)b->position + led > LED_AMOUNT) continue;
     
-    if(leds[led + b->position].b < brightness){
-      leds[led + b->position] = CHSV(0, 0, brightness);
+    if(leds[led + (int)b->position].b < brightness){
+      leds[led + (int)b->position] = CHSV(0, 0, brightness);
     }
 
   }
